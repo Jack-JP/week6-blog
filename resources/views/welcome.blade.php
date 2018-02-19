1eeac2749f6dd13c744ec7aa5fb8e991c8a5ead5
JvP <jvp@live.nl>
2018-02-19 10:36:34 +0100

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('title', 'Welcome')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card card-default">
+                <div class="card-header">Dashboard</div>
+                <div class="card-body">
+                    welkom page
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
