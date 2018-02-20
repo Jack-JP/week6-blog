f6639368673a4024e5d3add86bb14561d8e8e7fe
Riccardo Parrello <riccardoparrello@outlook.com>
2018-02-20 12:39:55 +0100

    [W3-007]
    Formatted text is now displayed correctly when reading posts.

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -20,7 +20,7 @@
             <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">
             <div class="card-body">
               <h2 class="card-title">{{$post->title}}</h2>
-              <p class="card-text">{{$post->body}}</p>
+              <p class="card-text">{!!$post->body!!}</p>
               <a href="#" class="btn btn-primary">Read More &rarr;</a>
             </div>
             <div class="card-footer text-muted">
